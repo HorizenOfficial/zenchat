774c609724d6890fac22c203373629ed5a031cea
Kendrick Tan <kendricktan0814@gmail.com>
2017-08-20 12:00:42 +1000

    env setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// Now we can render our application into it
+ReactDOM.render(<App />, document.getElementById('root'));
